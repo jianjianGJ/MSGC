ce0a2fc596430c5c741e335cde7efe963c65acff
jianjianGJ <1397919138@qq.com>
2024-04-16 20:01:42 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,14 @@
+from .gcn import GCN
+from .sgc import SGC, SGC_rich
+from .sage import SAGE
+from .appnp import APPNP
+from .gat import GAT
+__all__ = [
+    'GCN',
+    'SGC',
+    'SGC_rich',
+    'SAGE',
+    'APPNP',
+    'GAT'
+]
+#
